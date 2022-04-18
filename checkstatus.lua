a31352d89f43404e627e0609f1387489b185a1d6
vex <83321372+vexsxn@users.noreply.github.com>
2022-04-18 11:35:26 -0700

    Update checkstatus.lua

--- checkstatus.lua
+++ checkstatus.lua
@@ -1,7 +1,9 @@
+getgenv().CheckS = ""
+
 function CheckStatus()
     local W = game:GetService("ReplicatedStorage").WaveCounter
     local E = game:GetService("ReplicatedStorage").ProgressionCounter
-    if W.Value == 30 and E.Value == 0 then
+    if W.Value == 6 and E.Value == 4 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/vexsxn/dex/main/serverhop.lua",true))()
     end
 end
