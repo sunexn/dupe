222f7f88d1eb338005e0cedd131a7b4af04c0706
vex <83321372+vexsxn@users.noreply.github.com>
2022-04-18 22:41:04 -0700

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -1,22 +1,5 @@
-
-local B = game.Players.LocalPlayer.Backpack
-local P = game.Players.LocalPlayer.Character
-
-function DELETE()
-    for i,v in pairs(B:GetChildren()) do
-        if v.name == _G.T then
-            v.Parent = P
-        end
+for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+    if table.find(_G.T,v.Name) then
+        game:GetService("ReplicatedStorage").Remotes.drop:FireServer(v)
     end
-end
-
-_G.DEL = true
-
-while _G.DEL do
-DELETE()
-
-local ohInstance1 = P[_G.T]
-game:GetService("ReplicatedStorage").Remotes.drop:FireServer(ohInstance1)
-
-wait()
 end
