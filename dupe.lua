0c9fcf3ac3ed01efa9bf305a3132d30461471e6c
sun <83321372+sunexn@users.noreply.github.com>
2022-12-10 15:30:40 -0800

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -1,9 +1,1 @@
-There is a debate over the merits of homework and whether it is truly beneficial for students. Some studies have shown that too much homework can be detrimental to students' well-being and academic performance.
-
-One of the main negative effects of excessive homework is that it can lead to sleep deprivation. When students have a large amount of homework, they may stay up late trying to complete it, which can interfere with their natural sleep patterns. This can lead to a lack of sleep, which can have a range of negative effects on mental and physical health. Lack of sleep can cause irritability, difficulty concentrating, and a weakened immune system, among other things. It can also increase the risk of obesity, depression, and other health problems.
-
-Another potential negative effect of homework is that it can lead to academic stress and reduced enjoyment of school. When students have a lot of homework, it can feel overwhelming and can take away from their free time and extracurricular activities. This can lead to feelings of stress and pressure, which can affect their enjoyment of school and their overall motivation to learn. This can also lead to a lack of engagement in class and a decrease in academic performance.
-
-Furthermore, homework can perpetuate social and economic inequalities. Students from disadvantaged backgrounds may not have the same access to resources and support at home to help them with their homework. This can put them at a disadvantage compared to their peers and can contribute to the achievement gap between students from different backgrounds.
-
-In conclusion, while homework can have some positive effects on student learning, excessive homework can be detrimental to students' well-being and academic performance. It is important for educators to carefully consider the role of homework in their teaching and to strike a balance between its potential benefits and drawbacks.
+gone
